b67b5f49dc5f9986302f0654af3977deba837d17
cubean <cubean@warp-driven.com>
2023-06-19 01:55:22 +1000

    Added stg deploy

---
+++ devops/stg/0_init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir -p /data_lake/postgres-data
+
+mkdir -p /data_lake/zookeeper/zookeeper-datalog
+mkdir -p /data_lake/zookeeper/zookeeper-data
+mkdir -p /data_lake/zookeeper/zookeeper-logs
+
+mkdir -p /data_lake/object_storage
+mkdir -p /data_lake/clickhouse-data
+
+mkdir -p /data_lake/posthog/posthog
+mkdir -p /data_lake/posthog/docker
+
+cp -r ~/git/posthog/posthog/idl /data_lake/posthog/posthog/
+cp -r ~/git/posthog/docker/clickhouse /data_lake/posthog/docker/
---
+++ devops/stg/1_start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Starting the WarpDriven Data Tracking System!"
+docker-compose up -d
